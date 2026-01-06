d817052182a0ebe8a7726d34a04768627d6fcf2f
raytr <giaotran2001lk@gmail.com>
2026-01-06 17:00:18 +0700

    feat: practice destructuring props

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+
+function UseCard({props}) {
+    return (
+        <div>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+        </div>
+    );
+}
+
+export default UseCard
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UseCard from './components/Test.jsx'
+
+
+const user = {
+  name: "Ray",
+  age: 17,
+  city: "Hanoi"
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <UseCard props={user} />
   </StrictMode>,
 )

3a24120116ee1093149e06c648a8511a4ef5513a
raytr <giaotran2001lk@gmail.com>
2026-01-29 16:48:02 +0700

    chore: initialize a search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import "./SearchBar.css"
+
+export function WeatherSearch() {
+    return (
+        <div className="search-box">
+            <input type="text" placeholder="Search..."></input>
+            <button>Search</button>
+        </div>
+    )
+}
+
+
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,13 @@
 import './SearchPage.css'
 import NavBar from '../components/NavBar'
+import { WeatherSearch } from '../components/SearchBar'
 
 export function SearchPage() {
 
     return (
         <div>
             <NavBar />
+            <WeatherSearch />
         </div>
     )
 }
